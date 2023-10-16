bd15a1aef97f85a3eeb170a06ee5f926e1391837
Nicolas Medeiros <nick.medeiros.758660@gmail.com>
2023-10-16 14:52:54 -0300

    bugfix: background image com fundo branco

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,24 +4,39 @@ import Tecnologias from '@/components/Tecnologias'
 import Projetos from '@/components/Projetos'
 import Sobre from '@/components/Sobre'
 import Contatos from '@/components/Contato'
-
 import Footer from '@/components/Footer'
+
+import imgBg from '@/assets/background-code.jpg'
+
+
+FIXED: 'Corrigido erro na background em que o fundo ficava branco'
 const Home = () => {
   return (
     <>
-      <Topo />
+      <div
+        style={{
+          backgroundImage: `url('${imgBg.src}')`,
+          width: '100%',
+          height: '100%',
+          backgroundAttachment: 'fixed',
+          backgroundRepeat: 'repeat',
+          backgroundSize: 'cover'
+        }}
+      >
+        <Topo />
 
-      <Intro />
+        <Intro />
 
-      <Projetos />
+        <Projetos />
 
-      <Sobre />
+        <Sobre />
 
-      <Tecnologias />
+        <Tecnologias />
 
-      <Contatos />
+        <Contatos />
 
-      <Footer />
+        <Footer />
+      </div>
     </>
   )
 }
